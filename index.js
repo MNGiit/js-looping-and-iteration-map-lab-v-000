c06ac2e0a6f085228c2a7389caf895fc3cfd00f1
Mohammed Noori <kmanoorib@gmail.com>
2019-02-23 05:16:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,3 +22,12 @@ function attributesToPhrase(drivers) {
     
   });
 }
+
+    it('converts to list saying the name and where each individual is from', () => {
+      const drivers = [
+        { name: 'Bobby', hometown: 'Pittsburgh' },
+        { name: 'Sammy', hometown: 'New York' },
+        { name: 'Sally', hometown: 'Cleveland' },
+        { name: 'Annette', hometown: 'Los Angeles' },
+        { name: 'Bobby', hometown: 'Tampa Bay' }
+      ];
