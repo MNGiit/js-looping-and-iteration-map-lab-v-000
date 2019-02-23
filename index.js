5b660714a88b74b8deaf26dd996bebb1947b3c9e
Mohammed Noori <kmanoorib@gmail.com>
2019-02-23 05:02:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // Code your solution in this file.
-function lowerCaseDrivers(drivers) {
-  return drivers.map( function(driver) {return driver.toLowerCase();} );
-}
-
 // const map1 = array1.map(x => x * 2);
 
 // var doubles = numbers.map(function(num) {
 //  return num * 2;
 // });
+
+function lowerCaseDrivers(drivers) {
+  return drivers.map( function(driver) {return driver.toLowerCase();} );
+}
+
+function nameToAttributes() {
+  
+}
 
 
