448c17b5ba0cc606078b894e90417efa3c69c581
Mohammed Noori <kmanoorib@gmail.com>
2019-02-23 04:50:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers(arr) {
-  return arr.map(x.toLowerCase);
+  return arr.map(x => x.toLowerCase);
 }
 
 // const map1 = array1.map(x => x * 2);
