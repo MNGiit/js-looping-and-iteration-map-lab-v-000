f6b084a7aa0c20d9be08ac319fef024dfc20f914
Mohammed Noori <kmanoorib@gmail.com>
2019-02-23 04:55:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers) {
-  return drivers.map( function(driver) {return driver.toLowerCase;} );
+  drivers.map( function(driver) {return driver.toLowerCase;} );
 }
 
 // const map1 = array1.map(x => x * 2);
